db4ba1f1473aecafa9f6cb5a3cd2417ef2fc9f6d
kianfang <kianfang@users.noreply.github.com>
2023-05-30 15:21:26 +0000

    deploy: e72788367d86f5993cdb4d9acf031bdc6cbc6867

---
+++ js/common.mjs
@@ -0,0 +1,43 @@
+const $header = document.querySelector('#header');
+const $sidebar = document.querySelector('#sidebar');
+const $toc = document.querySelector('#content .toc');
+const headerHeight = $header.clientHeight;
+let lastScrollTop = 0;
+
+// 监听滚动事件, 使header随着滚动条的滚动而隐藏或显示
+window.addEventListener('scroll', function(e) {
+  // 当前滚动条距离顶部距离
+  const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  // 距离上次滚动后偏移的距离
+  const distance = currentScrollTop - lastScrollTop;
+  const transform = $header.style.transform;
+  const match = transform.match(/translateY\((\d+(\.\d+)?)px\)/);
+  // 当前header距离DOM顶部的距离
+  const translateY = match ? parseFloat(match[1], 10) : 0;
+  // 当滚动超过header高度后，header刚好隐藏的理论偏移的距离
+  const baseTop = Math.max(currentScrollTop - headerHeight, translateY);
+  // header 相对视口向上偏移量
+  let deltaY = Math.min(headerHeight, currentScrollTop - translateY);
+
+  if (distance < 0) {
+    // 向上滚动
+    const top = translateY >= currentScrollTop ? currentScrollTop : baseTop;
+    deltaY = currentScrollTop - top;
+
+    $header.style.transform = `translateY(${top}px)`;
+  } else {
+    // 向下移动
+  }
+
+  $header.style.opacity = 1 - deltaY / headerHeight;
+
+  if ($sidebar) {
+    $sidebar.style.transform = `translateY(${Math.min(headerHeight - deltaY, currentScrollTop)}px)`;
+  }
+
+  if($toc) {
+    $toc.style.transform = `translateY(${Math.min(headerHeight - deltaY, currentScrollTop)}px)`;
+  }
+
+  lastScrollTop = currentScrollTop;
+});
