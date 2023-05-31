9b279121a27f93c79376ed0771e1558e92a5f52e
kianfang <kianfang@users.noreply.github.com>
2023-05-31 03:52:53 +0000

    deploy: 122fc8941aca81899a4c886ed9e3790d16d52e17

--- js/common.mjs
+++ js/common.mjs
@@ -1,46 +1,56 @@
 const $header = document.querySelector('#header');
 const $sidebar = document.querySelector('#sidebar');
 const $toc = document.querySelector('#content .toc');
-const headerHeight = $header.clientHeight;
 let lastScrollTop = 0;
+let requestId = null;
 
 // 监听滚动事件, 使header随着滚动条的滚动而隐藏或显示
-window.addEventListener('scroll', function(e) {
-  // 当前滚动条距离顶部距离, iOS橡皮筋特性可以将滚动条拉到负数，这里校准一下，方便后续计算
-  const currentScrollTop = Math.max(window.pageYOffset || document.documentElement.scrollTop, 0);
-  // 距离上次滚动后偏移的距离
-  const distance = currentScrollTop - lastScrollTop;
-  const transform = $header.style.transform;
-  const match = transform.match(/translateY\((\d+(\.\d+)?)px\)/);
-  // 当前header距离DOM顶部的距离
-  const translateY = match ? parseFloat(match[1], 10) : 0;
-  // 当滚动超过header高度后，header刚好隐藏的理论偏移的距离
-  const baseTop = Math.max(currentScrollTop - headerHeight, translateY);
-  // header 相对视口向上偏移量
-  let deltaY = Math.min(headerHeight, currentScrollTop - translateY);
+window.addEventListener('scroll', function() {
+  // 当前滚动条距离顶部距离
+  const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-  if (distance < 0) {
-    // 向上滚动
-    // iOS橡皮筋特性，拉到底部可能会自动回弹
-    if (currentScrollTop + window.screen.height <= document.body.clientHeight) {
-      const top = translateY >= currentScrollTop ? currentScrollTop : baseTop;
-      deltaY = currentScrollTop - top;
-
-      $header.style.transform = `translateY(${top}px)`;
-    }
-  } else {
-    // 向下移动
+  // iOS橡皮筋特性, 可以将滚动条拉到负数，拉到底部可能会自动回弹
+  if (
+    currentScrollTop < 0
+    || currentScrollTop > document.body.clientHeight - window.innerHeight
+  ) {
+    return;
   }
 
-  $header.style.opacity = 1 - deltaY / headerHeight;
+  if(requestId) cancelAnimationFrame(requestId);
 
-  if ($sidebar) {
-    $sidebar.style.transform = `translateY(${Math.min(headerHeight - deltaY, currentScrollTop)}px)`;
-  }
+  requestId = compute(currentScrollTop);
 
-  if($toc) {
-    $toc.style.transform = `translateY(${Math.min(headerHeight - deltaY, currentScrollTop)}px)`;
-  }
-
-  lastScrollTop = currentScrollTop;
 });
+
+function compute(currentScrollTop) {
+  return requestAnimationFrame(() => {
+    // 距离上次滚动后偏移的距离
+    const distance = currentScrollTop - lastScrollTop;
+    const headerHeight = $header.clientHeight;
+    const transform = $header.style.transform;
+    const match = transform.match(/translateY\((-?\d+(\.\d+)?)px\)/);
+    // 当前header向DOM顶部便宜的距离（负数）
+    let deltaY = match ? parseFloat(match[1], 10) : 0;
+
+    if (distance < 0) {
+      // 向上滚动，header逐渐向下显示, deltaY趋近于0
+      deltaY = Math.min(deltaY - distance, 0);
+    } else {
+      // 向下移动，header逐渐向上隐藏, deltaY趋近于-headerHeight
+      deltaY = Math.max(deltaY - distance, -headerHeight);
+    }
+
+    $header.style.transform = `translateY(${deltaY}px)`;
+
+    if ($sidebar) {
+      $sidebar.style.transform = `translateY(${Math.min(headerHeight + deltaY, currentScrollTop)}px)`;
+    }
+
+    if ($toc) {
+      $toc.style.transform = `translateY(${Math.min(headerHeight + deltaY, currentScrollTop)}px)`;
+    }
+
+    lastScrollTop = currentScrollTop;
+  });
+}
